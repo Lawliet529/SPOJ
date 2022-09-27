df3ed38403bb57cb8cb688aff228bf7fc0f9ce0e
Lawliet529 <85656044+Lawliet529@users.noreply.github.com>
2022-09-27 13:06:46 +0700

    Add ReversePolishNotation.java

---
+++ src/ReversePolishNotation.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReversePolishNotation {
+
+  public static void main(String[] args) {
+    // Transform the algebraic expression with brackets into RPN form (Reverse Polish Notation).
+    // Two-argument operators: +, -, *, /, ^ (priority from the lowest to the highest), brackets
+    // ( ). Operands: only letters: a,b,...,z. Assume that there is only one RPN form (no
+    // expressions like a*b*c).
+    Scanner scan = new Scanner(System.in);
+    int t = scan.nextInt();
+    scan.nextLine();
+    for (int i = 0; i < t; i++) {
+      String s = scan.nextLine();
+      System.out.println(transform(s));
+    }
+    scan.close();
+  }
+
+  private static String transform(String s) {
+    StringBuilder sb = new StringBuilder();
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (Character.isLetter(c)) {
+        sb.append(c);
+      } else if (c == '(') {
+        stack.push(c);
+      } else if (c == ')') {
+        while (!stack.isEmpty() && stack.peek() != '(') {
+          sb.append(stack.pop());
+        }
+        stack.pop();
+      } else {
+        while (!stack.isEmpty() && priority(c) <= priority(stack.peek())) {
+          sb.append(stack.pop());
+        }
+        stack.push(c);
+      }
+    }
+    while (!stack.isEmpty()) {
+      sb.append(stack.pop());
+    }
+    return sb.toString();
+  }
+
+  private static int priority(char c) {
+    switch (c) {
+      case '+':
+      case '-':
+        return 1;
+      case '*':
+      case '/':
+        return 2;
+      case '^':
+        return 3;
+      default:
+        return 0;
+    }
+  }
+}
