ef47ebf3348308f6fab93a1f997ca593fd58c82c
Lawliet529 <85656044+Lawliet529@users.noreply.github.com>
2022-09-30 22:19:28 +0700

    Add AdaFriends.java

---
+++ src/AdaFriends.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class AdaFriends {
+
+  public static void main(String[] args) {
+    // Read input
+    Scanner scan = new Scanner(System.in);
+    int q = scan.nextInt();
+    int k = scan.nextInt();
+    scan.nextLine();
+    HashMap<String, Long> map = new HashMap<>();
+    for (int i = 0; i < q; i++) {
+      String name = scan.next();
+      map.put(name, map.getOrDefault(name, 0L) + scan.nextLong());
+      scan.nextLine();
+    }
+    scan.close();
+
+    // Extract all values and sort them
+    long[] values = new long[map.size()];
+    int i = 0;
+    for (long value : map.values()) {
+      values[i++] = value;
+    }
+    Arrays.sort(values);
+
+    // Compute the sum of the k last values
+    long sum = 0;
+    k = Math.min(k, values.length);
+    while (k-- > 0) {
+      sum += values[--i];
+    }
+    System.out.println(sum);
+  }
+}
