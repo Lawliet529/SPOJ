7bcb525aa55154f9d78cc38d06daa377f9d0e966
Lawliet529 <85656044+Lawliet529@users.noreply.github.com>
2022-10-19 09:54:42 +0700

    Use LinkedList for queue

--- src/Busland.java
+++ src/Busland.java
@@ -2,15 +2,14 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Busland {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedOutputStream bos = new BufferedOutputStream(System.out);
-    ArrayDeque<Integer> queue = new ArrayDeque<>();
+    LinkedList<Integer> queue = new LinkedList<>();
     int t = Integer.parseInt(br.readLine());
     for (int i = 0; i < t; i++) {
       bos.write(("Case " + (i + 1) + ":\n").getBytes());
@@ -20,36 +19,23 @@ public class Busland {
         switch (query[0]) {
           case "1":
             if (query[1].equals("B")) {
-              queue.add(Integer.parseInt(query[2]));
+              queue.addLast(Integer.parseInt(query[2]));
             } else {
-              queue.push(Integer.parseInt(query[2]));
+              queue.addFirst(Integer.parseInt(query[2]));
             }
             break;
           case "2":
             if (query[1].equals("B")) {
               queue.removeLast();
             } else {
-              queue.remove();
+              queue.removeFirst();
             }
             break;
           case "3":
             if (query[1].equals("D")) {
-              Iterator<Integer> iterator = queue.iterator();
-              int y = Integer.parseInt(query[2]);
-              for (int k = 1; k < y; k++) {
-                iterator.next();
-              }
-              bos.write((iterator.next() + "\n").getBytes());
+              bos.write((queue.get(Integer.parseInt(query[2]) - 1) + "\n").getBytes());
             } else {
-              Iterator<Integer> iterator = queue.iterator();
-              int index = 1;
-              while (iterator.hasNext()) {
-                if (iterator.next() == Integer.parseInt(query[2])) {
-                  bos.write((index + "\n").getBytes());
-                  break;
-                }
-                index++;
-              }
+              bos.write(((queue.indexOf(Integer.parseInt(query[2])) + 1) + "\n").getBytes());
             }
         }
       }
