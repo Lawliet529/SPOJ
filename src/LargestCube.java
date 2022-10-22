aeb1ae4d4d75e3101581405570c4cf3eb8a15340
Lawliet529 <85656044+Lawliet529@users.noreply.github.com>
2022-10-22 10:28:43 +0700

    Update LargestCube.java

--- src/LargestCube.java
+++ src/LargestCube.java
@@ -9,13 +9,15 @@ public class LargestCube {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedOutputStream bos = new BufferedOutputStream(System.out);
     int t = Integer.parseInt(br.readLine());
+    int[] cubes = new int[] {1, 8, 27, 64, 125, 216, 343, 512, 729, 1000, 1331, 1728, 2197, 2744, 3375, 4096, 4913, 5832, 6859, 8000, 9261};
     for (int i = 0; i < t; i++) {
       int n = Integer.parseInt(br.readLine());
-      int x = 1;
-      while (x * x * x <= n) {
-        x++;
+      int j = 0;
+      while (j < cubes.length && cubes[j] <= n) {
+        j++;
       }
-      bos.write((x - 1 + "\n").getBytes());
+      bos.write(String.valueOf(j).getBytes());
+      bos.write('\n');
     }
     bos.flush();
   }
